81b1298fd0e87a01150e77a1a274ba9ba93dc947
mattis dalleau <mattis.dalleau@epitech.eu>
2020-10-18 17:28:44 +0400

    created usage coloration along with errors error code O1 and added return for L2

--- main.py
+++ main.py
@@ -4,28 +4,41 @@ import sys
 import fileinput
 import re
 
+class color:
+    MAJOR   = '\033[91m'
+    MINOR   = '\033[92m'
+    INFO    = '\033[94m'
+    NORMAL  = '\033[0m'
+
+def usage():
+    print("Please use it as shown below :")
+    print("    ./cnormitek [folder]\n")
+    print("If you think this is an error please open an issue !")
+    exit()
+
 header_regex = (
-    r"\/\*\n"
-	r"\*\* EPITECH PROJECT, [0-9]{4}\n"
-	r"\*\* (.*)\n"
-	r"\*\* File description:\n"
-	r"\*\* (.*)\n"
-	r"\*\/"
-)
+        r"\/\*\n"
+        r"\*\* EPITECH PROJECT, [0-9]{4}\n"
+        r"\*\* (.*)\n"
+        r"\*\* File description:\n"
+        r"\*\* (.*)\n"
+        r"\*\/"
+        )
 
 errors = {
-    "F3": ("too many columns", "major"),
-    "G1": ("bad or missing header", "major"),
+        "F3": (color.MAJOR, "too many columns", "major"),
+        "G1": (color.MAJOR, "bad or missing header", "major"),
+        "O1": (color.MAJOR, "Your delivery folder should not contain unncessary files", "major"),
 
-    "C1": ("probably too many conditions nested", "minor"),
-    "C3": ("goto is discouraged", "minor"),
-    "G2": ("probably too many empty lines", "minor"),
-    "H2": ("no inclusion guard found", "minor"),
-    "L2": ("bad indentation", "minor"),
-    "L3": ("misplaced or missing space", "minor"),
+        "C1": (color.MINOR, "probably too many conditions nested", "minor"),
+        "C3": (color.MINOR, "goto is discouraged", "minor"),
+        "G2": (color.MINOR, "probably too many empty lines", "minor"),
+        "H2": (color.MINOR, "no inclusion guard found", "minor"),
+        "L2": (color.MINOR, "bad indentation", "minor"),
+        "L3": (color.MINOR, "misplaced or missing space", "minor"),
 
-    "implicit_L001": ("trailing space", "info")
-}
+        "implicit_L001": (color.INFO, "trailing space", "info")
+        }
 
 def check_file(file):
     content = ""
@@ -40,10 +53,10 @@ def check_file(file):
 def show_error(file, code, line = None):
     if line is None:
         line = "?"
-    
+
     error = errors[code]
-    
-    print(file + ":" + str(line) + "::" + code + " - " + error[0] + " (" + error[1] + ")")
+
+    print(file + ":" + str(line) + "::" + code + " - " + error[0] + error[1] + " (" + error[2] + ")" + color.NORMAL)
 
 def check_G1(file, content):
     matches = re.search(header_regex, content)
@@ -81,13 +94,13 @@ def check_lines(file):
         if re.search('(\t|    ){2,}\}?\s*(else if)', line):
             show_error(file, "C1", line_nb)
 
-            
+
         # goto
         if "goto " in line:
             show_error(file, "C3", line_nb)
 
         # misplaced spaces
-        if re.search('(while|for|if)\(', line):
+        if re.search('(while|for|if|return)\(', line):
             show_error(file, "L3", line_nb)
 
         # in while/for/if/...
@@ -107,7 +120,7 @@ def check_lines(file):
             empty_lines_count += 1
         else:
             empty_lines_count = 0
-        
+
         if empty_lines_count >= 2:
             show_error(file, "G2", line_nb)
 
@@ -121,10 +134,17 @@ def read_dir(dir):
         if os.path.isfile(dir + "/" + file):
             if re.search('\.(c|h)$', file):
                 check_file(dir + "/" + file)
+            elif re.search('\.(o|sh|a|so|d|gcda|gcno|out|swp|elf|obj)$', file):
+                show_error(file, "O1", 0)
+
         elif not (file == "tests" and os.path.exists(dir + "/.git")):
             read_dir(dir + "/" + file)
 
 argdir = sys.argv
+
+if not len(sys.argv) > 1:
+    usage()
+
 if os.path.isfile(argdir[1]):
     check_file(argdir[1])
 else:
